28f3f73618c4b52277f4a776856a5d9ba2846ce8
Birendra Kumar Das <birendradascse@gmail.com>
2021-11-15 16:45:21 +0530

    pages re-added

--- first_app/config/routes.rb
+++ first_app/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'home/about'
+  
+  resources :products
+  #get 'home/index'
+  #get 'home/about'
+
+  resources :shops, only:[:index, :show]
+
+  root 'shops#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

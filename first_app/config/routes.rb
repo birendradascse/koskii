f382856277da4c4337f01fbccf6fe734eaa9eefe
Birendra Kumar Das <birendradascse@gmail.com>
2021-11-16 02:06:18 +0530

    new added pages

--- first_app/config/routes.rb
+++ first_app/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   
+  get 'cards/show'
   resources :products
   #get 'home/index'
   #get 'home/about'
 
   resources :shops, only:[:index, :show]
+  resources :order_items
+  resource :cards, only:[:show]
+  resource :cart, only:[:show]
 
   root 'shops#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- first_app/db/schema.rb
+++ first_app/db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_075947) do
+ActiveRecord::Schema.define(version: 2021_11_15_122610) do
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "product_id"
+    t.integer "order_id"
+    t.decimal "total"
+    t.decimal "unit_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal "subtotal"
+    t.decimal "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
